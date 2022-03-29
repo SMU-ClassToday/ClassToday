49804998e09b38b554b5cccc2523db9b6ef9961c
YesCoach <sbsii1020@gmail.com>
2022-03-29 21:15:42 +0900

    feat: Project 생성 및 Code-base 설정 - 스토리보드 제거 - SceneDelegate에서 Code-base UI를 위한 설정 추가

---
+++ ClassToday/ClassToday/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ClassToday
+//
+//  Created by 박태현 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+}
+
