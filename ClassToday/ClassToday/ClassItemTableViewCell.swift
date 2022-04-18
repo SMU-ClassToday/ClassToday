418a672ac0ca3db0c49f5d92b528c4299882dc6c
poohyhy <poohyhy98@gmail.com>
2022-04-19 02:30:13 +0900

    feat : tabbar, main화면 구현
    - tabbar와 main화면 테이블뷰 구현

---
+++ ClassToday/ClassToday/ClassItemTableViewCell.swift
@@ -0,0 +1,88 @@
+//
+//  ClassItemTableViewCell.swift
+//  ClassToday
+//
+//  Created by poohyhy on 2022/04/19.
+//
+
+import UIKit
+import SnapKit
+
+class ClassItemTableViewCell: UITableViewCell {
+    
+    static let identifier = "ClassItemTableViewCell"
+    
+    private lazy var thumbnailView: UIImageView = {
+        let thumbnailView = UIImageView()
+        thumbnailView.backgroundColor = .secondarySystemBackground
+        thumbnailView.layer.cornerRadius = 4.0
+        return thumbnailView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "6월 수학 가형 모고풀이"
+        titleLabel.font = .systemFont(ofSize: 16.0, weight: .semibold)
+        return titleLabel
+    }()
+    
+    private lazy var locationLabel: UILabel = {
+        let locationLabel = UILabel()
+        locationLabel.text = "노원구 중계 1동"
+        locationLabel.font = .systemFont(ofSize: 14.0, weight: .regular)
+        return locationLabel
+    }()
+    
+    private lazy var dateDiffLabel: UILabel = {
+        let dateDiffLabel = UILabel()
+        dateDiffLabel.text = " | 1분 전"
+        dateDiffLabel.font = .systemFont(ofSize: 14.0, weight: .regular)
+        return dateDiffLabel
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let priceLabel = UILabel()
+        priceLabel.text = "20,000원"
+        priceLabel.font = .systemFont(ofSize: 16.0, weight: .bold)
+        return priceLabel
+    }()
+    
+    private func layout() {
+        [
+            thumbnailView,
+            titleLabel,
+            locationLabel,
+            dateDiffLabel,
+            priceLabel
+        ].forEach { contentView.addSubview($0) }
+        
+        let commonInset: CGFloat = 16.0
+        
+        thumbnailView.snp.makeConstraints {
+            $0.leading.top.bottom.equalToSuperview().inset(commonInset)
+            $0.width.equalTo(thumbnailView.snp.height)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(thumbnailView.snp.trailing).offset(commonInset)
+            $0.top.equalTo(thumbnailView.snp.top)
+            $0.trailing.equalToSuperview().inset(commonInset)
+        }
+        locationLabel.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+        }
+        dateDiffLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationLabel.snp.trailing)
+            $0.top.equalTo(locationLabel.snp.top)
+        }
+        priceLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationLabel.snp.leading)
+            $0.top.equalTo(locationLabel.snp.bottom).offset(8.0)
+            $0.trailing.equalToSuperview().inset(commonInset)
+        }
+    }
+    
+    func setupView() {
+        layout()
+    }
+}
---
+++ ClassToday/ClassToday/TabbarController.swift
@@ -0,0 +1,86 @@
+//
+//  TabbarController.swift
+//  ClassToday
+//
+//  Created by poohyhy on 2022/04/19.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+    //+탭을 알럿을 모달하는 버튼으로 사용할때 필요한 flag
+    var isUploadTabBarEnabled: Bool = true
+    
+    //MARK: UI Components
+    lazy var alertController: UIAlertController = {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alertController.view.tintColor = UIColor(red: 93/255, green: 95/255, blue: 239/255, alpha: 1.0)
+        
+        let buyUploadAction = UIAlertAction(title: "구매글 작성", style: .default, handler: nil)
+        let sellUploadAction = UIAlertAction(title: "판매글 작성", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        [buyUploadAction, sellUploadAction, cancelAction].forEach {
+            alertController.addAction($0)
+        }
+        
+        return alertController
+    }()
+    
+    //viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.tintColor = UIColor(red: 93/255, green: 95/255, blue: 239/255, alpha: 1.0)
+        delegate = self
+        
+        let viewControllerList: [UIViewController] = Tabbar.allCases.map {
+            let viewController = $0.viewController
+            viewController.tabBarItem = $0.tabBarItem
+            return viewController
+        }
+        
+        viewControllers = viewControllerList
+    }
+}
+
+extension TabbarController: UITabBarControllerDelegate {
+    //+탭 선택시 얼럿을 표시
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item.image == Icon.plus.image {
+            present(alertController, animated: true)
+            isUploadTabBarEnabled = false
+        } else {
+            isUploadTabBarEnabled = true
+        }
+    }
+    
+    //+탭 선택시 뷰컨 보여주지 않음
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return isUploadTabBarEnabled
+    }
+}
+
+
+// MARK: - MapVc
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+// MARK: - ChatVC
+class ChatViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+// MARK: - ProfileVC
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
