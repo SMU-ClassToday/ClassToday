626f1ae711824c3758196e68ddfd1dbeb89d8f65
YesCoach <sbsii1020@gmail.com>
2022-04-18 15:38:21 +0900

    feat: Navigation 계층 추가 및 등록하기 버튼 구현 - 등록하기 화면 진입을 상위 뷰 구현

--- ClassToday/ClassToday/SceneDelegate.swift
+++ ClassToday/ClassToday/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let mainViewController = ViewController()
+        let mainViewController = UINavigationController(rootViewController: MainViewController())
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
     }
