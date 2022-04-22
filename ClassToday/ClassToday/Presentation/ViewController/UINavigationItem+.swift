542dae876a0a91da4a53977ddfa9cddcfab8cad8
prettyturtle <unifconti@gmail.com>
2022-04-23 02:13:08 +0900

    feat: UINavigationItem+ 에 왼쪽 상단에 네비게이션 타이틀을 만드는 메서드 구현

--- ClassToday/ClassToday/Presentation/ViewController/UINavigationItem+.swift
+++ ClassToday/ClassToday/Presentation/ViewController/UINavigationItem+.swift
@@ -1,8 +1,18 @@
 //
 //  UINavigationItem+.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/15.
 //
 
-import Foundation
+import UIKit
+
+extension UINavigationItem {
+    /// 네비게이션 바의 왼쪽 타이틀을 설정하는 메서드
+    func setLeftTitle(text: String) {
+        let label = UILabel()
+        label.text = text
+        label.font = .systemFont(ofSize: 20.0, weight: .semibold)
+        self.leftBarButtonItem = UIBarButtonItem(customView: label)
+    }
+}
