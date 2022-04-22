f01cedce181f1def96d962eff22489a01a0a189a
YesCoach <sbsii1020@gmail.com>
2022-04-22 16:06:30 +0900

    refactor: TextField 터치 로직 수정 및 폴더링 - ScrollView 추가에 따른 화면 Touch 동작 리팩토링 - 프로젝트 파일 폴더링

--- ClassToday/ClassToday/ViewController/MainViewController.swift
+++ ClassToday/ClassToday/ViewController/MainViewController.swift
@@ -35,11 +35,11 @@ class MainViewController: UIViewController {
         let actionSheet = UIAlertController(title: "글을 등록하시겠습니까?", message: nil, preferredStyle: .actionSheet)
         let enrollForSale = UIAlertAction(title: "수업판매글", style: .default) { [weak self] action in
             guard let self = self else { return }
-            self.navigationController?.pushViewController(ViewController(), animated: true)
+            self.navigationController?.pushViewController(ClassItemEnrollViewController(), animated: true)
         }
         let enrollForBuy = UIAlertAction(title: "수업구매글", style: .default) { [weak self] action in
             guard let self = self else { return }
-            self.navigationController?.pushViewController(ViewController(), animated: true)
+            self.navigationController?.pushViewController(ClassItemEnrollViewController(), animated: true)
         }
         actionSheet.addAction(enrollForBuy)
         actionSheet.addAction(enrollForSale)
