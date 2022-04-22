8a741e8e4ec69cf09ed4dae7f8708b5bb0b9ebae
prettyturtle <unifconti@gmail.com>
2022-04-23 02:04:20 +0900

    feat: ProfileUserInfoTableViewCell 구현

--- ClassToday/ClassToday/Presentation/ViewController/ProfileUserInfoTableViewCell.swift
+++ ClassToday/ClassToday/Presentation/ViewController/ProfileUserInfoTableViewCell.swift
@@ -1,8 +1,40 @@
 //
 //  ProfileUserInfoTableViewCell.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/19.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ProfileUserInfoTableViewCell: UITableViewCell {
+    
+    let user: User
+    let type: UserProfileType
+    
+    lazy var infoView = ProfileUserInfoView(user: user, type: type)
+    
+    init(user: User, type: UserProfileType) {
+        self.user = user
+        self.type = type
+        super.init(style: .default, reuseIdentifier: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        layout()
+    }
+}
+
+private extension ProfileUserInfoTableViewCell {
+    func layout() {
+        contentView.addSubview(infoView)
+        infoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
