1ac4c10aeb57dc7cd5d35a64b6c41e9346958d85
prettyturtle <unifconti@gmail.com>
2022-04-23 02:02:41 +0900

    feat: Option 구현

--- ClassToday/ClassToday/Presentation/ViewController/Option.swift
+++ ClassToday/ClassToday/Presentation/ViewController/Option.swift
@@ -1,8 +1,36 @@
 //
 //  Option.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/18.
 //
 
-import Foundation
+import UIKit
+
+enum Option: String, CaseIterable {
+    case location = "내 위치 설정"
+    case category = "관심 카테고리 설정"
+    case notice = "공지사항"
+    case QnA = "QnA"
+    case question = "문의"
+    case setting = "설정"
+    
+    var viewController: UIViewController {
+        switch self {
+        case .location:
+            return UIViewController()
+        case .category:
+            return UIViewController()
+        case .notice:
+            return UIViewController()
+        case .QnA:
+            return UIViewController()
+        case .question:
+            return UIViewController()
+        case .setting:
+            return SettingViewController()
+        }
+    }
+    
+    var text: String { self.rawValue }
+}
