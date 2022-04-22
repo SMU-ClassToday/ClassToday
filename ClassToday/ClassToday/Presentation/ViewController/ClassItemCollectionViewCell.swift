2ddcd6157e9063377e765148d13b334196368f29
prettyturtle <unifconti@gmail.com>
2022-04-23 02:11:22 +0900

    feat: ClassItemCollectionViewCell 구현

--- ClassToday/ClassToday/Presentation/ViewController/ClassItemCollectionViewCell.swift
+++ ClassToday/ClassToday/Presentation/ViewController/ClassItemCollectionViewCell.swift
@@ -1,8 +1,34 @@
 //
 //  ClassItemCollectionViewCell.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/19.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ClassItemCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ClassItemCollectionViewCell"
+    
+    private lazy var thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .secondarySystemBackground
+        imageView.layer.cornerRadius = 4.0
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    func setupView() {
+        layout()
+    }
+}
+
+private extension ClassItemCollectionViewCell {
+    func layout() {
+        contentView.addSubview(thumbnailImageView)
+        thumbnailImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
