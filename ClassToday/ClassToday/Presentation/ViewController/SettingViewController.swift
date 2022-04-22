276afa5d1a148c2e0b61325b4574d7cb53b55bfe
prettyturtle <unifconti@gmail.com>
2022-04-23 02:00:56 +0900

    feat: SettingViewController 구현

--- ClassToday/ClassToday/Presentation/ViewController/SettingViewController.swift
+++ ClassToday/ClassToday/Presentation/ViewController/SettingViewController.swift
@@ -1,8 +1,76 @@
 //
 //  SettingViewController.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/18.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SettingViewController: UIViewController {
+    // MARK: - UI Components
+    private lazy var settingTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.estimatedRowHeight = 100.0
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        tableView.register(
+            OptionTableViewCell.self,
+            forCellReuseIdentifier: OptionTableViewCell.identifier
+        )
+        return tableView
+    }()
+    
+    // MARK: - Properties
+    private let settingOptions: [Setting] = Setting.allCases
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        attribute()
+        layout()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SettingViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return settingOptions.count
+    }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: OptionTableViewCell.identifier,
+            for: indexPath
+        ) as? OptionTableViewCell else { return UITableViewCell() }
+        let option = settingOptions[indexPath.row]
+        cell.setupView(option: option.text)
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+// MARK: - UI Methods
+private extension SettingViewController {
+    func setupNavigationBar() {
+        navigationController?.navigationBar.topItem?.backButtonTitle = ""
+        navigationItem.title = "설정"
+    }
+    func attribute() {
+        view.backgroundColor = .systemBackground
+    }
+    func layout() {
+        view.addSubview(settingTableView)
+        settingTableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
