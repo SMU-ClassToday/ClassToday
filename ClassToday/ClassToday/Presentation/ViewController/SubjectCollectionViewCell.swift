2d6377d0761e61cf95494eb4feb7aa5fa35fb026
prettyturtle <unifconti@gmail.com>
2022-04-23 02:09:47 +0900

    feat: SubjectCollectionViewCell 구현

--- ClassToday/ClassToday/Presentation/ViewController/SubjectCollectionViewCell.swift
+++ ClassToday/ClassToday/Presentation/ViewController/SubjectCollectionViewCell.swift
@@ -1,8 +1,41 @@
 //
 //  SubjectCollectionViewCell.swift
-//  ClassToday
+//  Practice
 //
-//  Created by yc on 2022/04/23.
+//  Created by yc on 2022/04/19.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SubjectCollectionViewCell: UICollectionViewCell {
+    static let identifier = "SubjectCollectionViewCell"
+    
+    private lazy var subjectLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = .mainColor
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    func setupView(subject: Subject, fontSize: CGFloat) {
+        layout()
+        setupSubjectLabel(subject: subject.text, fontSize: fontSize)
+    }
+}
+
+private extension SubjectCollectionViewCell {
+    func setupSubjectLabel(subject: String ,fontSize: CGFloat) {
+        subjectLabel.text = subject
+        subjectLabel.font = .systemFont(ofSize: fontSize, weight: .semibold)
+        subjectLabel.layer.cornerRadius = fontSize
+    }
+    func layout() {
+        contentView.addSubview(subjectLabel)
+        subjectLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
