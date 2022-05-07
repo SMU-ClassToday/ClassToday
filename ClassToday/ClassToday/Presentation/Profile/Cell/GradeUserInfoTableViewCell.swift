84db7a8a2ea0ac8ef3608919e6bc76de34eff22b
prettyturtle <unifconti@gmail.com>
2022-05-08 00:05:07 +0900

    refactor: GradeStarView를 이용해 유저의 평점 정보를 보여줌
    - 공통 UI를 만든 후 적용한 것이므로 이전의 UI와 동일

--- ClassToday/ClassToday/Presentation/Profile/Cell/GradeUserInfoTableViewCell.swift
+++ ClassToday/ClassToday/Presentation/Profile/Cell/GradeUserInfoTableViewCell.swift
@@ -15,46 +15,7 @@ class GradeUserInfoTableViewCell: UITableViewCell {
         label.font = .systemFont(ofSize: 16.0, weight: .medium)
         return label
     }()
-    private lazy var starImageView1: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "star")
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .mainColor
-        return imageView
-    }()
-    private lazy var starImageView2: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "star")
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .mainColor
-        return imageView
-    }()
-    private lazy var starImageView3: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "star")
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .mainColor
-        return imageView
-    }()
-    private lazy var starImageView4: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "star")
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .mainColor
-        return imageView
-    }()
-    private lazy var starImageView5: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "star")
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .mainColor
-        return imageView
-    }()
-    private lazy var starImageStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.spacing = 8.0
-        return stackView
-    }()
+    private lazy var gradeStarView = GradeStarView(grade: 4.79019)
     
     func setupView() {
         layout()
@@ -64,20 +25,8 @@ class GradeUserInfoTableViewCell: UITableViewCell {
 private extension GradeUserInfoTableViewCell {
     func layout() {
         [
-            starImageView1,
-            starImageView2,
-            starImageView3,
-            starImageView4,
-            starImageView5
-        ].forEach { button in
-            button.snp.makeConstraints {
-                $0.size.equalTo(36.0)
-            }
-            starImageStackView.addArrangedSubview(button)
-        }
-        [
             titleLabel,
-            starImageStackView
+            gradeStarView
         ].forEach { contentView.addSubview($0) }
         
         let commonInset: CGFloat = 16.0
@@ -85,9 +34,10 @@ private extension GradeUserInfoTableViewCell {
         titleLabel.snp.makeConstraints {
             $0.leading.top.trailing.equalToSuperview().inset(commonInset)
         }
-        starImageStackView.snp.makeConstraints {
+        gradeStarView.snp.makeConstraints {
             $0.leading.equalTo(titleLabel.snp.leading)
             $0.top.equalTo(titleLabel.snp.bottom).offset(commonInset)
+            $0.trailing.equalTo(titleLabel.snp.trailing)
             $0.bottom.equalToSuperview().inset(commonInset)
         }
     }
