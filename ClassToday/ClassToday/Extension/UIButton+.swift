2372afdded07917e4d5c934ef5bc11da0acd6068
prettyturtle <unifconti@gmail.com>
2022-06-01 19:10:07 +0900

    refactor: PR Review를 반영한 수정
    - GradeStarView에 반복되는 UI, UIImageView+ Extension으로 수정
    - 외부에서 호출이 없는 프로퍼티들을 private로 변경

---
+++ ClassToday/ClassToday/Extension/UIButton+.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+.swift
+//  ClassToday
+//
+//  Created by yc on 2022/06/01.
+//
+
+import Foundation
---
+++ ClassToday/ClassToday/Presentation/Auth/LaunchSignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInLaunchViewController.swift
+//  ClassToday
+//
+//  Created by yc on 2022/06/01.
+//
+
+import UIKit
+
+class SignInLaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
