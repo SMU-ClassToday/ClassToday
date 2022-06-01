4b004e9203bd299cbd5c88b10dffd174bebcc699
prettyturtle <unifconti@gmail.com>
2022-06-01 19:58:41 +0900

    feat: 로그인 초기 화면 구현
    - 소셜 로그인 버튼
    - 이메일로 회원가입 화면으로 이동하는 버튼
    - 로그인 화면으로 이동하는 버튼

--- ClassToday/ClassToday/Extension/UIButton+.swift
+++ ClassToday/ClassToday/Extension/UIButton+.swift
@@ -5,4 +5,24 @@
 //  Created by yc on 2022/06/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    func makeSNSStyleButton(
+        title: String,
+        image: UIImage?,
+        titleColor: UIColor,
+        tintColor: UIColor,
+        backgroundColor: UIColor
+    ) {
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .semibold)
+        self.setImage(image, for: .normal)
+        self.tintColor = tintColor
+        self.layer.cornerRadius = 4.0
+        self.backgroundColor = backgroundColor
+        self.imageEdgeInsets = UIEdgeInsets(top: 0.0, left: -5.0, bottom: 0.0, right: 0.0)
+        self.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: -5.0)
+    }
+}
