94596a00fad80c1ece156e49c126c796087d6ccf
YesCoach <sbsii1020@gmail.com>
2022-07-03 03:05:58 +0900

    feat: Category 상태 및 선택 UI 구현 - ClassCategoryCollectionView를 재사용하여 UI 구현

--- ClassToday/ClassToday/Presentation/Map/MapViewController.swift
+++ ClassToday/ClassToday/Presentation/Map/MapViewController.swift
@@ -18,26 +18,29 @@ class MapViewController: UIViewController {
         leftTitle.font = .systemFont(ofSize: 20.0, weight: .bold)
         return leftTitle
     }()
-
+    
     private lazy var starItem: UIBarButtonItem = {
         let starItem = UIBarButtonItem.menuButton(self, action: #selector(didTapStarButton), image: Icon.star.image)
         return starItem
     }()
-
+    
     private lazy var searchItem: UIBarButtonItem = {
         let searchItem = UIBarButtonItem.menuButton(self, action: #selector(didTapSearchButton), image: Icon.search.image)
         return searchItem
     }()
-
+    
     private func setupNavigationBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftTitle)
         navigationItem.rightBarButtonItems = [starItem, searchItem]
     }
     
     //MARK: - Views
-    private lazy var categoryCollectionView: DetailContentCategoryView = {
-        let collectionView = DetailContentCategoryView()
-        return collectionView
+    private lazy var categoryView: MapCategoryView = {
+        let categoryView = MapCategoryView(frame: .zero)
+        categoryView.categoryCollectionView.dataSource = self
+        categoryView.categoryCollectionView.delegate = self
+        categoryView.delegate = self
+        return categoryView
     }()
     
     private lazy var mapView: NMFNaverMapView = {
@@ -49,11 +52,22 @@ class MapViewController: UIViewController {
         mapView.positionMode = NMFMyPositionMode.direction
         return naverMapView
     }()
-
+    
+    //MARK: - Properties
     private var curLocation: Location? {
         return LocationManager.shared.getCurrentLocation()
     }
-
+    private var delegate: MapCategorySelectViewControllerDelegate?
+    private var data: [CategoryItem] = [] {
+        willSet {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.categoryView.setPlaceHolderLabel(newValue.isEmpty)
+            }
+        }
+    }
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
@@ -68,14 +82,20 @@ class MapViewController: UIViewController {
         setupMapView(location: location)
     }
     
+    //MARK: - Methods
     private func setupLayout() {
+        view.addSubview(categoryView)
         view.addSubview(mapView)
+        categoryView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
+        }
         mapView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(categoryView.snp.bottom)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
-
+    
     private func setupMapView(location: Location) {
         let coord = NMGLatLng(lat: location.lat, lng: location.lon)
         mapView.mapView.latitude = coord.lat
@@ -90,6 +110,51 @@ extension MapViewController {
     }
     @objc private func didTapSearchButton(sender: UIButton) {
         
+    }
+}
+
+// MARK: - CollectionViewDataSource
+
+extension MapViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: DetailContentCategoryCollectionViewCell.identifier,
+            for: indexPath) as? DetailContentCategoryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configureWith(category: data[indexPath.item])
+        return cell
+    }
+}
+
+// MARK: - CollectionViewDelegateFlowLayout
+
+extension MapViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let fontsize = data[indexPath.item].description.size(withAttributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .medium)])
+        let width = fontsize.width
+        let height = fontsize.height
+        return CGSize(width: width + 24, height: height)
+    }
+}
+
+extension MapViewController: MapCategoryViewDelegate {
+    func pushCategorySelectViewController() {
+        let vc = MapCategorySelectViewController()
+        vc.delegate = self
+        let selectedCategory = data.map { $0 as? Subject }.compactMap { $0 }
+        vc.configure(with: Set(selectedCategory))
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension MapViewController: MapCategorySelectViewControllerDelegate {
+    func passData(subjects: Set<Subject>) {
+        data = Array(subjects)
     }
 }
 
