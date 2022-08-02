9158b6549c829585bf94f724f1df374b4f4fbf32
poohyhy <poohyhy98@gmail.com>
2022-08-02 21:07:26 +0900

    feat: 채팅 내 이미지 전송 기능 구현
    - 채팅에서 이미지를 전송하는 기능을 구현했습니다.

--- ClassToday/ClassToday/Presentation/Chat/Model/ImageMediaItem.swift
+++ ClassToday/ClassToday/Presentation/Chat/Model/ImageMediaItem.swift
@@ -9,14 +9,14 @@ import UIKit
 import MessageKit
 
 struct ImageMediaItem: MediaItem {
-  var url: URL?
-  var image: UIImage?
-  var placeholderImage: UIImage
-  var size: CGSize
-
-  init(image: UIImage) {
-    self.image = image
-    self.size = CGSize(width: 240, height: 240)
-    self.placeholderImage = UIImage()
-  }
+    var url: URL?
+    var image: UIImage?
+    var placeholderImage: UIImage
+    var size: CGSize
+    
+    init(image: UIImage) {
+        self.image = image
+        self.size = CGSize(width: 240, height: 240)
+        self.placeholderImage = UIImage()
+    }
 }
