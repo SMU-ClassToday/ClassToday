c0c752b394d2199c54290a02fdf8718b885a69c2
yescoach <sbsii1020@gmail.com>
2022-08-10 17:12:17 +0900

    feat: 수업 Detail VC에 유저 정보 추가 - ClassItem의 writer 정보로 유저 셀 구성 - 유저 셀 클릭시 해당 유저의 detail VC로 이동

--- ClassToday/ClassToday/Model/User.swift
+++ ClassToday/ClassToday/Model/User.swift
@@ -5,7 +5,7 @@
 //  Created by yc on 2022/03/29.
 //
 
-import Foundation
+import UIKit
 
 struct User: Codable, Equatable {
     var id: String = UUID().uuidString
@@ -20,6 +20,24 @@ struct User: Codable, Equatable {
     var stars: [String]?
     let subjects: [Subject]?
     let chatItems: [String]?
+    
+    func thumbnailImage(completion: @escaping (UIImage?) -> Void) {
+        guard let profileImageURL = profileImage else {
+            return completion(nil)
+        }
+        if let cachedImage = ImageCacheManager.shared.object(forKey: profileImageURL as NSString) {
+            completion(cachedImage)
+        }
+        StorageManager.shared.downloadImage(urlString: profileImageURL) { result in
+            switch result {
+            case .success(let image):
+                ImageCacheManager.shared.setObject(image, forKey: profileImageURL as NSString)
+                completion(image)
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }
+    }
     
     static func == (lhs: User, rhs: User) -> Bool {
         return lhs.id == rhs.id
--- ClassToday/ClassToday/Presentation/ClassDetail/View/ClassDetailTableView/DetailUserCell.swift
+++ ClassToday/ClassToday/Presentation/ClassDetail/View/ClassDetailTableView/DetailUserCell.swift
@@ -8,5 +8,96 @@
 import UIKit
 
 class DetailUserCell: UITableViewCell {
+    // MARK: - View
+    private lazy var userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "person")
+        imageView.layer.cornerRadius = 30
+        return imageView
+    }()
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16.0, weight: .medium)
+        return label
+    }()
+    private lazy var companyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .regular)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    private lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .regular)
+        return label
+    }()
+    
+    // MARK: - Properties
+    static var identifier = "DetailUserCell"
+    private var user: User?
+    private var completion: ((User) -> ())?
 
+    // MARK: - Initialize
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let border = CALayer()
+        let width = 0.5
+        border.frame = CGRect.init(x: 0, y: rect.height - width, width: rect.width, height: width)
+        border.backgroundColor = UIColor.separator.cgColor
+        self.layer.addSublayer(border)
+        super.draw(rect)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let user = user else { return }
+        completion?(user)
+    }
+
+    // MARK: - Methods
+    private func setUpLayout() {
+        [userImageView, userNameLabel, companyLabel, locationLabel].forEach {
+            contentView.addSubview($0)
+        }
+        userImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview().inset(16)
+            $0.width.height.equalTo(60)
+        }
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(userImageView)
+            $0.leading.equalTo(userImageView.snp.trailing).offset(16)
+        }
+        companyLabel.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(userNameLabel)
+        }
+        locationLabel.snp.makeConstraints {
+            $0.top.equalTo(companyLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(userNameLabel)
+            $0.bottom.equalTo(userImageView)
+        }
+    }
+
+    func configure(with user: User, completion: @escaping (User) -> ()) {
+        self.user = user
+        self.completion = completion
+        userNameLabel.text = user.nickName
+        companyLabel.text = user.company
+        locationLabel.text = user.location?.name
+        DispatchQueue.main.async { [weak self] in
+            user.thumbnailImage { image in
+                guard let self = self,
+                      let image = image else { return }
+                self.userImageView.image = image
+            }
+        }
+    }
 }
